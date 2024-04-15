f9ee0ff3ccd778bb44e40a7f00928677865c8fc7
Ziyad Parkar <143216700+Ziyad007@users.noreply.github.com>
2024-04-15 09:21:40 +0530

    Add files via upload

--- Assignment-3/Code/index.js
+++ Assignment-3/Code/index.js
@@ -1,9 +1,38 @@
- const express=require('express');
- 
- const app=express();
+const dbConnect=require('./mongodb')
+const express=require('express');
+const app=express();
+app.use(express.json())
 
- app.use(express.static('public'))
+//get API
 
- app.listen(4000,()=>{
-    console.log("Server is started");
- })                  
+app.get('/',async(req,res)=>{
+    let result=await dbConnect();
+    result=await result.find().toArray();
+    res.send(result);
+})
+
+//post API
+app.post('/',async(req,res)=>{
+    let result=await dbConnect();
+    result=await result.insertOne(req.body);
+    res.send("Data Inserted Successfully");
+
+})
+
+//put API
+app.put('/:name',async(req,res)=>{
+    let result=await dbConnect();
+    result=await result.updateOne({name:req.params.name},{$set:req.body});
+    res.send("Data Updated Successfully")
+})
+
+
+
+//delete API
+app.delete('/:name',async(req,res)=>{
+    let result=await dbConnect();
+    result=await result.deleteOne({name:req.params.name})
+    res.send("Data Deleted Successfully");
+})
+
+app.listen(3000);
